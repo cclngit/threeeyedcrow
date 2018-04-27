763208f6c2a119492e3050b11ebfe68eff51ab25
cclngit <38783129+cclngit@users.noreply.github.com>
2018-04-27 14:30:51 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -16,7 +16,7 @@ bot.on('message', function (message) {
 
 bot.on('message', function (message){
     if (message.content == '!information') {
-        message.reply('Tout d abord bienvenu a toi ! si tu as remarqué il y a plusieur élément qui sont l Eau, la Terre, le Feu, le Vent et la Foudre pour les obtenir c est trés simple il suffi de taper "!" suivi de l élément en minuscule le tout coller donc dès maintenant tu peux essayer ça dans le salon (général-🐥), exemple : { "!eau" Attribut l élément Eau💧 } { "!feu" Attribut l élément Feu 🔥 } { "!foudre" Attribut l élément Foudre ⚡️} { "!terre" Attribut l élément Terre 🌳} { "!vent" Attribut l élément Vent 💨 }')
+        message.reply('Tout d abord bienvenu a toi ! si tu as remarqué, il y a plusieur élément qui sont l Eau, la Terre, le Feu, le Vent et la Foudre pour les obtenir c est trés simple il suffit de taper "!" suivi de l élément en minuscule le tout coller donc dès maintenant tu peux essayer ça dans le salon (général-🐥), exemple : { "!eau" Attribut l élément Eau💧 } { "!feu" Attribut l élément Feu 🔥 } { "!foudre" Attribut l élément Foudre ⚡️} { "!terre" Attribut l élément Terre 🌳} { "!vent" Attribut l élément Vent 💨 }')
     }
 })
 
